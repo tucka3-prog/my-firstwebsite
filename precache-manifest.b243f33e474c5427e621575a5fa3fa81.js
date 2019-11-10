636aa4f92012e2011260e29a8591541680d3b459
tucka3-prog <erikaslaptevas@gmail.com>
2019-11-10 16:34:53 +0200

    Updates

---
+++ precache-manifest.b243f33e474c5427e621575a5fa3fa81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083701db23275f795bcec382adeac375",
+    "url": "/my-firstwebsite/index.html"
+  },
+  {
+    "revision": "0f4702000bccf234df28",
+    "url": "/my-firstwebsite/static/css/main.01269fb4.chunk.css"
+  },
+  {
+    "revision": "31be9304f256db5a9cad",
+    "url": "/my-firstwebsite/static/js/2.bd4e5b95.chunk.js"
+  },
+  {
+    "revision": "0f4702000bccf234df28",
+    "url": "/my-firstwebsite/static/js/main.15c5acff.chunk.js"
+  },
+  {
+    "revision": "d37004e97a2619cc916b",
+    "url": "/my-firstwebsite/static/js/runtime-main.1c60f55e.js"
+  }
+]);
